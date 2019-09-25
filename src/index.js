4808be0ba068a55e020f6ae0131a24ea64a550fb
Martin Kruger <martin@martinkruger.com>
2019-09-25 12:10:47 +0200

    New direction, update of old setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/*
+  Information holder – an object designed to know certain information and provide that information to other objects.
+  Structurer – an object that maintains relationships between objects and information about those relationships.
+  Service provider – an object that performs specific work and offers services to others on demand.
+  Controller – an object designed to make decisions and control a complex task.
+  Coordinator – an object that doesn’t make many decisions but, in a rote or mechanical way, delegates work to other objects.
+  Interfacer – an object that transforms information or requests between distinct parts of a system.
+*/
+
+
 import './assets/css/normalize.css';
 import './assets/css/style.css';
 import './assets/css/acrylic.css';
